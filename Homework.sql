c96690a7e7f8353278c6fc38a68cbbcc5e655427
KSolo0203 <jr.solovich@gmail.com>
2023-07-02 20:00:10 +0300

    4rd HW

--- Homework.sql
+++ Homework.sql
@@ -1,54 +1,55 @@
-/* Задачи 1-6.
-1. Отсортируйте данные по полю заработная плата (salary) в порядке: убывания; возрастания
-2. Выведите 5 максимальных заработных плат (salary)
-3. Посчитайте суммарную зарплату (salary) по каждой специальности (роst)
-4. Найдите кол-во сотрудников с специальностью (post) «Рабочий» в возрасте от 24 до 49 лет включительно.
-5. Найдите количество специальностей
-6. Выведите специальности, у которых средний возраст сотрудников меньше 30 лет
+USE lesson_4;
+
+/* Задача 1.
+Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
 */
-USE lesson_3;
-
--- Пункт 1.
-SELECT firstname, lastname, age, post, salary, seniority
-FROM staff
-ORDER BY salary DESC
+SELECT COUNT(id) AS 'Лайков суммарно от юзеров младше 12 лет'
+FROM likes l INNER JOIN profiles p ON l.user_id = p.user_id
+WHERE TIMESTAMPDIFF(YEAR, p.birthday, CURDATE()) < 12
 ;
 
-SELECT firstname, lastname, age, post, salary, seniority
-FROM staff
-ORDER BY salary --ASC by default
+/* Задача 2.
+Определить кто больше поставил лайков (всего): мужчины или женщины.
+*/
+SELECT  
+	CASE gender
+		WHEN 'f' THEN 'Женщины'
+		ELSE 'Мужчины'
+    END AS 'Лайков больше поставили'
+FROM likes l INNER JOIN profiles p ON l.user_id = p.user_id
+GROUP BY gender
+ORDER BY COUNT(id) DESC LIMIT 1
 ;
 
--- Пункт 2.
-SELECT firstname, lastname, post, salary
-FROM staff
-ORDER BY salary DESC
-LIMIT 5
+/* Задача 3.
+Вывести всех пользователей, которые не отправляли сообщения.
+*/
+SELECT id, firstname, lastname
+FROM users
+WHERE id NOT IN
+(SELECT from_user_id
+FROM messages 
+GROUP BY from_user_id)
 ;
 
--- Пункт 3.
-SELECT post, SUM(salary)
-FROM staff
-GROUP BY post
-;
-
--- Пункт 4.
-SELECT COUNT(id) as 'Количество рабочих от 24 до 49 лет'
-FROM staff
-WHERE post = 'Рабочий' AND age BETWEEN 24 AND 49
-;
-
--- Пункт 5.
-SELECT COUNT(*) as 'Количество специальностей'
-FROM
-(SELECT post
-FROM staff
-GROUP BY post) as list
-;
-
--- Пункт 6.
-SELECT post--, AVG(age)
-FROM staff
-GROUP BY post
-HAVING AVG(age) < 30 -- удалить эту строку и раскоментить, чтобы убедиться воочию
+/* Задача 4.
+(по желанию)* Пусть задан некоторый пользователь. Из всех друзей этого пользователя
+найдите человека, который больше всех написал ему сообщений.
+*/
+SELECT CONCAT(u.id, ' ', u.firstname, ' ', u.lastname) as 'Лучший друг!'
+FROM users u RIGHT JOIN
+(SELECT from_user_id
+FROM messages m INNER JOIN
+(SELECT initiator_user_id as 'friends_ids'
+FROM friend_requests frq
+WHERE target_user_id = 1 AND status = 'approved' -- заменить INT на подзапрос, возвращающий users.id нужного человека
+UNION
+SELECT target_user_id  as 'friends_ids'
+FROM friend_requests frq
+WHERE initiator_user_id = 1 AND status = 'approved') -- заменить INT на подзапрос, возвращающий users.id нужного человека
+AS friends_ids ON m.from_user_id = friends_ids.friends_ids
+WHERE to_user_id = 1 -- заменить INT на подзапрос, возвращающий users.id нужного человека
+GROUP BY from_user_id
+ORDER BY COUNT(id) DESC LIMIT 1)
+AS best_friend_id ON u.id = best_friend_id.from_user_id
 ;
