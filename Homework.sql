3474b456070c25e0ee87c92c09a01727ad38cac6
KSolo0203 <jr.solovich@gmail.com>
2023-07-09 11:49:00 +0300

    5th hw done

--- Homework.sql
+++ Homework.sql
@@ -1,55 +1,52 @@
+-- Для решения задач используйте базу данных lesson_4.
 USE lesson_4;
 
 /* Задача 1.
-Подсчитать общее количество лайков, которые получили пользователи младше 12 лет.
+Создайте представление, в которое попадет информация о пользователях (имя, фамилия, город и пол),
+которые не старше 20 лет.
 */
-SELECT COUNT(id) AS 'Лайков суммарно от юзеров младше 12 лет'
-FROM likes l INNER JOIN profiles p ON l.user_id = p.user_id
-WHERE TIMESTAMPDIFF(YEAR, p.birthday, CURDATE()) < 12
+CREATE or REPLACE VIEW users_younger as
+SELECT
+	u.firstname,
+	u.lastname,
+    p.hometown,
+    p.gender
+from users u inner join profiles p on u.id = p.user_id
+WHERE TIMESTAMPDIFF(YEAR, p.birthday, CURDATE()) < 20
 ;
 
 /* Задача 2.
-Определить кто больше поставил лайков (всего): мужчины или женщины.
+Найдите кол-во отправленных каждым пользователем сообщений и выведите ранжированный список пользователей,
+указав имя и фамилию пользователя, количество отправленных сообщений и место в рейтинге
+(первое место у пользователя с максимальным количеством сообщений). Используйте DENSE_RANK.
 */
-SELECT  
-	CASE gender
-		WHEN 'f' THEN 'Женщины'
-		ELSE 'Мужчины'
-    END AS 'Лайков больше поставили'
-FROM likes l INNER JOIN profiles p ON l.user_id = p.user_id
-GROUP BY gender
-ORDER BY COUNT(id) DESC LIMIT 1
+WITH cte AS (SELECT
+	u.id,
+    u.firstname,
+    u.lastname,
+    count(m.id) AS count_m
+FROM users u INNER JOIN messages m ON u.id = m.from_user_id
+GROUP BY u.id)
+SELECT
+	id,
+    CONCAT(firstname, ' ' , lastname) as 'User name',
+    count_m,
+    DENSE_RANK() OVER (ORDER BY count_m DESC) AS 'rank'
+FROM cte
 ;
 
 /* Задача 3.
-Вывести всех пользователей, которые не отправляли сообщения.
+Выберите все сообщения, отсортируйте сообщения в хронологическом порядке
+и найдите разницу дат отправления между соседними сообщениями, получившегося списка.
+Используйте LEAD или LAG.
 */
-SELECT id, firstname, lastname
-FROM users
-WHERE id NOT IN
-(SELECT from_user_id
-FROM messages 
-GROUP BY from_user_id)
-;
-
-/* Задача 4.
-(по желанию)* Пусть задан некоторый пользователь. Из всех друзей этого пользователя
-найдите человека, который больше всех написал ему сообщений.
-*/
-SELECT CONCAT(u.id, ' ', u.firstname, ' ', u.lastname) as 'Лучший друг!'
-FROM users u RIGHT JOIN
-(SELECT from_user_id
-FROM messages m INNER JOIN
-(SELECT initiator_user_id as 'friends_ids'
-FROM friend_requests frq
-WHERE target_user_id = 1 AND status = 'approved' -- заменить INT на подзапрос, возвращающий users.id нужного человека
-UNION
-SELECT target_user_id  as 'friends_ids'
-FROM friend_requests frq
-WHERE initiator_user_id = 1 AND status = 'approved') -- заменить INT на подзапрос, возвращающий users.id нужного человека
-AS friends_ids ON m.from_user_id = friends_ids.friends_ids
-WHERE to_user_id = 1 -- заменить INT на подзапрос, возвращающий users.id нужного человека
-GROUP BY from_user_id
-ORDER BY COUNT(id) DESC LIMIT 1)
-AS best_friend_id ON u.id = best_friend_id.from_user_id
+SELECT
+	id,
+    from_user_id,
+    to_user_id,
+    created_at,
+    -- body,
+    TIMESTAMPDIFF(MINUTE, lag(created_at) over (ORDER BY created_at), created_at) AS 'Time after last message',
+    TIMESTAMPDIFF(MINUTE, created_at, lead(created_at) over (ORDER BY created_at)) AS 'Time before next message'
+FROM messages
 ;
